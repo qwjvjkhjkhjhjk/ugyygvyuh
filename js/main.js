f8a058e72ffec8677e8e91e7a95b74b44120ade6
qwjvjkhjkhjhjk <ramezhany@forms.illustrav.com>
2024-10-31 17:48:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 60
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+    function sendEmailToTelegram() {
+        const email = document.getElementById('emailInput').value;
+        
+        if (email) {
+            const token = '5991588711:AAGHuQCpjWdvHgMgmTSMyBpskdvLfmgcgxg'; // حط التوكن الخاص بالبوت هنا
+            const chatId = '@jdjxjxjpppp'; // حط الـ chat ID هنا
+            
+            const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=New-Email: ${email}`;
+            
+            fetch(url)
+                .then(response => {
+                    if (response.ok) {
+                        alert("Email sent successfully!");
+                    } else {
+                        alert("Failed to send email.");
+                    }
+                })
+                .catch(error => console.error("Error:", error));
+        } else {
+            alert("Please enter a valid email.");
+        }
+    }
